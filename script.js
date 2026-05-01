de8b05aae390437314f17075c247fc544078868c
Learamo <teddy.learamo@student.moringaschool.com>
2026-05-01 09:57:25 +0300

    add elements to script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("search-form");
+const input = document.getElementById("word-input");
+const resultDiv = document.getElementById("result");
+const errorDiv = document.getElementById("error");
+
+
