ae54e2eae12a506cfabbfdbbcbe22783c7e823e8
Learamo <teddy.learamo@student.moringaschool.com>
2026-05-01 09:58:42 +0300

    add event listener to script.js file

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ const input = document.getElementById("word-input");
 const resultDiv = document.getElementById("result");
 const errorDiv = document.getElementById("error");
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  const word = input.value.trim();
+
+  // validation
+  if (word === "") {
+    errorDiv.textContent = "Please enter a word";
+    resultDiv.innerHTML = "";
+    return;
+  }
+
+  fetchWord(word);
+});
