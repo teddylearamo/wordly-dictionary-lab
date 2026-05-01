f84bce566fc0ec8c47e8a6ef87ad02e2bc547076
Learamo <teddy.learamo@student.moringaschool.com>
2026-05-01 10:53:17 +0300

    finalized project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Wordly Dictionary App
+
+## Description
+This is a Single Page Application (SPA) that allows users to search for word definitions using a dictionary API.
+
+## Features
+- Search for a word
+- Displays pronunciation, definition, and example
+- Handles errors for invalid input
+- Updates page dynamically (no reload)
+
+## How to Run
+1. Open index.html in your browser
+2. Enter a word and click search
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Fetch API
+
+## FUTURE IMPROVEMENTS
+- add audio pronunciaton
+- add synonym list
+- save search history
+
+## API USED
+-Free dictionary API: htps://api.dictionryapi.dev/
+
--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const input = document.getElementById("word-input");
 const resultDiv = document.getElementById("result");
 const errorDiv = document.getElementById("error");
 
+const toggleBtn = document.getElementById("theme-toggle");
+
 // EVENT LISTENER
 
 form.addEventListener("submit", function (e) {
@@ -70,3 +72,14 @@ function displayResult(data) {
 document.body.style.backgroundColor = "#68b86f";
 
 }
+
+// DARK MODE TOGGLE
+toggleBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️ Light Mode";
+  } else {
+    toggleBtn.textContent = "🌙 Dark Mode";
+  }
+});
